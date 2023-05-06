29bcdc146f811c87b1e9cbb032465a4a836dcd25
Kristina Gorkovskaya <kristina.gorkovskaya@gmail.com>
2023-05-06 09:56:20 +0100

    Implements methods in DataExtractor and DatabaseConnecor classes

--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,39 @@
+from database_utils import DatabaseConnector
+import pandas as pd
+from sqlalchemy import text
+
 
 class DataExtractor:
-    pass
+
+    @staticmethod
+    def read_rds_table(db_connector, table_name):
+        '''Read all rows from table into Pandas dataframe.
+
+        Arguments:
+            db_connector: DatabaseConnector
+            table_name: string
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            query = f"SELECT * from {table_name};"
+            with db_connector.engine.connect() as con:
+                df = pd.read_sql_query(sql=text(query), con=con)
+                return df
+
+        except Exception as err:
+            print(f"Failed to read from table {table_name}")
+            print(f"{err.__class__.__name__}: {err}")
+            return pd.DataFrame()
+
+
+if __name__ == '__main__':
+
+    db_connector = DatabaseConnector()
+    db_connector.init_db_engine()
+
+    df = DataExtractor.read_rds_table(db_connector, 'legacy_users')
+    print(df.shape)
+    print(df.head())
--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,76 @@
+from sqlalchemy import create_engine, inspect, text
+import yaml
+
 
 class DatabaseConnector:
-    pass
+
+    def __init__(self):
+        self.engine = None
+
+    @staticmethod
+    def read_db_creds():
+        '''Reads database credentials from YAML file.
+
+        Returns: 
+            dict
+        '''
+
+        try:
+            with open("db_creds.yaml", "r") as file:
+                return yaml.safe_load(file)
+        except Exception as err:
+            print("Failed to read database credentials")
+            print(f"{err.__class__.__name__}: {err}")
+            return dict()
+
+    def init_db_engine(self, db_type='postgresql', db_api='psycopg2'):
+        '''Initializes a SQLAlchemy database engine.
+
+        Args: 
+            db_type: string
+            db_api: string
+
+        Returns:
+            SQLAlchemy database engine or None.
+        '''
+
+        try:
+            db_creds = self.read_db_creds()
+            user = db_creds['RDS_USER']
+            password = db_creds['RDS_PASSWORD']
+            host = db_creds['RDS_HOST']
+            port = db_creds['RDS_PORT']
+            database = db_creds['RDS_DATABASE']
+
+            cxn_string = f"{db_type}+{db_api}://{user}:{password}@{host}:{port}/{database}"
+            self.engine = create_engine(cxn_string)
+            return self.engine
+
+        except Exception as err:
+            print("Failed to establish database connection")
+            print(f"{err.__class__.__name__}: {err}")
+            return None
+
+    def list_db_tables(self):
+        '''List tables on database.
+
+        Returns:
+            list
+        '''
+        try:
+            inspector = inspect(self.engine)
+            table_names = inspector.get_table_names()
+            return table_names
+        except Exception as err:
+            print("Failed to get table names")
+            print(f"{err.__class__.__name__}: {err}")
+            return []
+
+
+if __name__ == '__main__':
+
+    db_connector = DatabaseConnector()
+    engine = db_connector.init_db_engine()
+    engine.read_sql_table()
+    table_names = db_connector.list_db_tables()
+    print(table_names)
