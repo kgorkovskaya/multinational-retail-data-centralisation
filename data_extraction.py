4039da0e5cb8911517d1fb6bd74e952b919e4e6c
Kristina Gorkovskaya <kristina.gorkovskaya@gmail.com>
2023-05-07 11:01:05 +0100

    Implements time_it decorator in data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -3,7 +3,7 @@ AiCore Multinational Retail Data Centralisation Project
 Data extraction
 
 Author: Kristina Gorkovskaya
-Date: 2023-05-06
+Date: 2023-05-07
 '''
 
 
@@ -11,6 +11,20 @@ import pandas as pd
 import tabula
 from database_utils import DatabaseConnector
 from sqlalchemy import text
+from time import time
+
+
+def time_it(func):
+    '''Decorator for timing execution of methods, and printing
+    the number of records (if method returns a dataframe)'''
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        if isinstance(result, pd.core.frame.DataFrame):
+            print(f'{len(result):,} records loaded')
+        print(f'Run time: {time() - start}')
+        return result
+    return wrapper
 
 
 class DataExtractor:
@@ -24,6 +38,7 @@ class DataExtractor:
     '''
 
     @staticmethod
+    @time_it
     def read_rds_table(db_connector, table_name):
         '''Read all rows from database table into Pandas dataframe.
 
@@ -40,7 +55,6 @@ class DataExtractor:
             query = f"SELECT * from {table_name};"
             with db_connector.engine.connect() as con:
                 df = pd.read_sql_query(sql=text(query), con=con)
-                print(f'Records loaded: {len(df):,}')
                 return df
 
         except Exception as err:
@@ -49,6 +63,7 @@ class DataExtractor:
             return pd.DataFrame()
 
     @staticmethod
+    @time_it
     def retrieve_pdf_data(url):
         '''Read PDF file into a Pandas dataframe.
 
@@ -62,8 +77,7 @@ class DataExtractor:
         print(f'Reading PDF data from url: {url}')
         try:
             df = tabula.read_pdf(url, pages='all')
-            df = pd.concat(df)
-            print(f'Records loaded: {len(df):,}')
+            df = pd.concat(df).reset_index(drop=True)
             return df
 
         except Exception as err:
