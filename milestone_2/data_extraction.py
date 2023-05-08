1c621ed94704fbf27434545a248264c2d7a00108
Kristina Gorkovskaya <kristina.gorkovskaya@gmail.com>
2023-05-08 12:33:46 +0100

    Moves milestone 2 files to mielstone_2 directory

---
+++ milestone_2/data_extraction.py
@@ -0,0 +1,244 @@
+'''
+AiCore Multinational Retail Data Centralisation Project
+Data extraction
+
+Author: Kristina Gorkovskaya
+Date: 2023-05-07
+'''
+
+import boto3
+import io
+import pandas as pd
+import re
+import requests
+import tabula
+from database_utils import DatabaseConnector
+from sqlalchemy import text
+from utils import print_newline, time_it
+
+
+class DataExtractor:
+    '''This class extracts data from a variety of input sources
+    into a Pandas dataframe. Input sources:
+        - Database tables
+        - JSON
+        - PDF files
+        - API
+
+    Attributes:
+        None
+    '''
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def extract_from_s3(path):
+        '''Download CSV data from S3 bucket; create Pandas DataFrame.
+
+        Read data directly into a DataFrame without downloading the file locally.
+
+        Arguments:
+            path (string): full path to file on S3
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            print('Downloading file from S3: ' + path)
+            s3_client = boto3.client('s3')
+            path_split = re.split(r'/+', path)
+            bucket = path_split[1]
+            key = path_split[-1]
+            obj = s3_client.get_object(Bucket=bucket, Key=key)
+            df = pd.read_csv(io.BytesIO(obj['Body'].read()))
+            return df
+        except Exception as err:
+            print(f'Failed to retrieve store details')
+            print(f'{err.__class__.__name__}: {err}')
+            return pd.DataFrame()
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def list_number_of_stores(url, headers=dict()):
+        '''Retrive number of stores from API.
+
+        Arguments:
+            url (str): URL of API endpoint
+            headers (dict): headers for GET request
+
+        Returns:
+            int: number of stores
+        '''
+
+        try:
+            print('Getting number of stores from API: ' + url)
+            r = DataExtractor.send_get_request(url, headers)
+            num_stores = r.json()['number_stores']
+            print(f'\t{num_stores:,} stores')
+            return num_stores
+        except Exception as err:
+            print(f'Failed to get number of stores')
+            print(f'{err.__class__.__name__}: {err}')
+            return 0
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def read_json(url, headers=dict()):
+        '''Read JSON data from URL into Pandas dataframe.
+
+        Arguments:
+            url (string): URL of JSON file
+            headers (dict): headers for GET request
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            print('Reading JSON data: ' + url)
+            r = DataExtractor.send_get_request(url, headers)
+            df = pd.DataFrame(r.json())
+            return df
+        except Exception as err:
+            print(f'Failed to get number of stores')
+            print(f'{err.__class__.__name__}: {err}')
+            return pd.DataFrame()
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def read_rds_table(db_connector, table_name):
+        '''Read all rows from database table into Pandas dataframe.
+
+        Arguments:
+            db_connector (DatabaseConnector): contains a SQLAlchemy engine for executing queries
+            table_name: string
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            db_name = db_connector.engine.url.database
+            print(f'Reading data from RDS table: {db_name}.dbo.{table_name}')
+            query = f"SELECT * from {table_name};"
+            with db_connector.engine.connect() as con:
+                df = pd.read_sql_query(sql=text(query), con=con)
+                return df
+
+        except Exception as err:
+            print(f'Failed to read data')
+            print(f'{err.__class__.__name__}: {err}')
+            return pd.DataFrame()
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def retrieve_pdf_data(url):
+        '''Read PDF file into a Pandas dataframe.
+
+        Arguments:
+            url (string): location of PDF file
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            print('Reading PDF data from url: ' + url)
+            df = tabula.read_pdf(url, pages='all')
+            df = pd.concat(df).reset_index(drop=True)
+            return df
+
+        except Exception as err:
+            print(f'Failed to read data')
+            print(f'{err.__class__.__name__}: {err}')
+            return pd.DataFrame()
+
+    @staticmethod
+    @time_it
+    @print_newline
+    def retrieve_stores_data(endpoint, headers, num_stores=1):
+        '''Retrieve store details from API.
+
+        Arguments:
+            endpoint (str): URL of API endpoint
+            headers (dict): headers for GET request
+            num_stores (int): number of stores to retrieve
+
+        Returns:
+            Pandas DataFrame
+        '''
+
+        try:
+            print('Retrieving store details from API: ' + endpoint)
+            store_data = []
+            for store_no in range(num_stores):
+                url = endpoint.format(store_no=store_no)
+                r = DataExtractor.send_get_request(url, headers)
+                store_data.append(r.json())
+            df = pd.DataFrame(store_data)
+            return df
+        except Exception as err:
+            print(f'Failed to retrieve store details')
+            print(f'{err.__class__.__name__}: {err}')
+            return pd.DataFrame()
+
+    @staticmethod
+    def send_get_request(url, headers=dict()):
+        '''Send HTTP GET request
+
+        Arguments:
+            url (string)
+            headers (dict): request headers (optional)
+
+        Returns:
+            Requests object instance
+        '''
+
+        r = requests.get(url, headers=headers)
+        assert r.status_code == 200, f'Request failed with status {r.status_code}'
+        return r
+
+
+if __name__ == '__main__':
+
+    db_connector = DatabaseConnector()
+    db_connector.init_db_engine()
+
+    print('\n')
+    users = DataExtractor.read_rds_table(db_connector, 'legacy_users')
+    print(users.head())
+
+    print('\n')
+    url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+    df = DataExtractor.retrieve_pdf_data(url)
+    print(df.head())
+
+    print('\n')
+    url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+    headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+    num_stores = DataExtractor.list_number_of_stores(url, headers)
+
+    print('\n')
+    url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_no}'
+    df = DataExtractor.retrieve_stores_data(url, headers, num_stores)
+    print(df.head())
+
+    print('\n')
+    url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_no}'
+    df = DataExtractor.retrieve_stores_data(url, headers, num_stores)
+    print(df.head())
+
+    print('\n')
+    s3_address = 's3://data-handling-public/products.csv'
+    df = DataExtractor.extract_from_s3(s3_address)
+    print(df.head())
+
+    print('\n')
+    url = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
+    df = DataExtractor.read_json(url)
+    print(df.head())
---
+++ milestone_2/database_utils.py
@@ -0,0 +1,126 @@
+'''
+AiCore Multinational Retail Data Centralisation Project
+Database utilities
+
+Author: Kristina Gorkovskaya
+Date: 2023-05-06
+'''
+
+from sqlalchemy import create_engine, inspect
+import yaml
+
+
+class DatabaseConnector:
+    '''This class connects to a database using SQLAlchemy.
+
+    Attributes:
+        engine (Engine): SQLAlchemy Engine, connected to DB
+        file_name (str): path to YAML file containing connection details and
+            login credentials
+    '''
+
+    def __init__(self, file_name='db_creds.yaml'):
+        '''See help(DatabaseConnector) for accurate signature.'''
+
+        self.engine = None
+        self.file_name = file_name
+
+    def read_db_creds(self):
+        '''Read database credentials from YAML file.
+
+        Returns: 
+            dict
+        '''
+
+        print(f'Loading database credentials from file: {self.file_name}')
+        try:
+            with open(self.file_name, 'r') as file:
+                return yaml.safe_load(file)
+        except Exception as err:
+            print('Failed to read database credentials')
+            print(f'{err.__class__.__name__}: {err}')
+            return dict()
+
+    def init_db_engine(self, db_type='postgresql', db_api='psycopg2'):
+        '''Initialize a SQLAlchemy Engine object.
+
+        Arguments: 
+            db_type (str): database type
+            db_api (str): database API
+
+        Returns: 
+            SQLAlchemy database engine or None.
+        '''
+
+        try:
+            db_creds = self.read_db_creds()
+            user = db_creds['RDS_USER']
+            password = db_creds['RDS_PASSWORD']
+            host = db_creds['RDS_HOST']
+            port = db_creds['RDS_PORT']
+            database = db_creds['RDS_DATABASE']
+
+            print(f'Connecting to database: {database}')
+            cxn_string = f'{db_type}+{db_api}://{user}:{password}@{host}:{port}/{database}'
+            self.engine = create_engine(cxn_string)
+            return self.engine
+
+        except Exception as err:
+            print('Failed to establish database connection')
+            print(f'{err.__class__.__name__}: {err}')
+            return None
+
+    def list_db_tables(self):
+        '''List tables on database.
+
+        Returns: 
+            list of table names
+        '''
+
+        try:
+            inspector = inspect(self.engine)
+            table_names = inspector.get_table_names()
+            return table_names
+        except Exception as err:
+            print('Failed to get table names')
+            print(f'{err.__class__.__name__}: {err}')
+            return []
+
+    def print_db_tables(self):
+        '''Pretty-print list of tables on database.
+        '''
+        print(f'\nTables in {self.engine.url.database}:')
+        tables = self.list_db_tables()
+        print('\t' + '\n\t'.join(sorted(tables)))
+
+    def upload_to_db(self, df, table_name, if_exists='replace'):
+        '''Upload Pandas dataframe to a table in the database.
+
+        Arguments:
+            df (Pandas Dataframe): data to upload
+            table_name (str): name of table being created
+            if_exists (str): how to behave if table exists 
+                ('fail', 'replace', or 'append')
+
+        Returns:
+            None
+        '''
+
+        try:
+            db_name = self.engine.url.database
+            print(
+                f'Writing data to RDS table: {db_name}.dbo.{table_name}')
+            msg = 'Incorrect if_exists parameter passed'
+            assert if_exists in ['fail', 'replace', 'append'], msg
+            df.to_sql(table_name, self.engine, if_exists=if_exists)
+        except Exception as err:
+            print(f'Failed to upload table {table_name} to database')
+            print(f'{err.__class__.__name__}: {err}')
+
+
+if __name__ == '__main__':
+
+    db_connector = DatabaseConnector()
+    engine = db_connector.init_db_engine()
+    table_names = db_connector.list_db_tables()
+    print(table_names)
